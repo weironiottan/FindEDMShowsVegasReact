3b59139e4f0e0a7be69776c807221244bd121a5f
weironiottan <chrisgabrielsson@protonmail.com>
2021-10-22 19:21:47 -0700

    adding temp changes

--- find-edm-shows/src/components/header/header.js
+++ find-edm-shows/src/components/header/header.js
@@ -8,7 +8,7 @@ function Header() {
     <section className="hero-section bg-auto bg-center bg-no-repeat  h-2/5 w-full flex flex-col justify-between"
         style={{backgroundImage: `url(${heroImage})` }}>
             <div className="self-center mt-5 p-5 bg-gray-900 bg-opacity-30 rounded-3xl backdrop-filter backdrop-blur-sm font-bold 
-            filter drop-shadow-3xl text-7xl tracking-widest uppercase font-mono text-white">find edm shows in vegas</div>
+            filter drop-shadow-3xl  text-base sm:text-7xl tracking-widest uppercase font-mono text-white">find edm shows in vegas</div>
         <section className="navbar h-20 px-0 bg-gray-900 bg-opacity-50  backdrop-filter backdrop-blur-sm  flex justify-center items-center">
             <SearchBox />
         </section>
