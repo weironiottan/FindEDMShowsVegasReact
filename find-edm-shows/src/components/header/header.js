3508a1f9067310253f3635c27178a2af1c2d4d1b
Christian Gabrielsson <christiangabrielsson@Christians-MacBook-Pro-2.local>
2021-10-28 14:26:34 -0700

    reseized images

--- find-edm-shows/src/components/header/header.js
+++ find-edm-shows/src/components/header/header.js
@@ -5,7 +5,7 @@ function Header() {
 
     return(
     <>
-    <section className="hero-section bg-auto bg-center bg-no-repeat  h-2/5 w-full flex flex-col justify-between"
+    <section className="hero-section bg-cover bg-center bg-no-repeat  h-2/5 w-full flex flex-col justify-between"
         style={{backgroundImage: `url(${heroImage})` }}>
             <div className="self-center mt-5 p-5 bg-gray-900 bg-opacity-30 rounded-3xl backdrop-filter backdrop-blur-sm font-bold 
             filter drop-shadow-3xl  text-xl sm:text-7xl tracking-widest uppercase font-mono text-white">find edm shows in vegas</div>
