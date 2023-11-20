10eade992c2067f176906f60bfa6379bd0ba1e93
Christian Gabrielsson <chrisgabrielsson@protonmail.com>
2023-11-20 12:29:23 -0700

    updated url and updated react, tailwind a whole host of things

--- find-edm-shows/craco.config.js
+++ find-edm-shows/craco.config.js
@@ -1,11 +1,21 @@
 // craco.config.js
+// module.exports = {
+//     style: {
+//       postcss: {
+//         plugins: [
+//           require('tailwindcss'),
+//           require('autoprefixer'),
+//         ],
+//       },
+//     },
+//   }
+/* eslint-disable */
+const tailwindcss = require("tailwindcss");
+
 module.exports = {
     style: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
-    },
-  }
+        postcssOptions: {
+            plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')]
+        }
+    }
+};
